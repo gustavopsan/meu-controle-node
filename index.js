77b2fd520520637c384f7cf825c6282a04b2c45c
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-07-17 12:23:47 -0300

    Ajustando problema de requisições com CORS

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-    app.use(cors());
+    app.use(CORS());
     next();
 });
 
